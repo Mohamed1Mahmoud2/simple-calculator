184c4a1880d7f4f9ed16260a39639f90e8bec7c7
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-11-28 20:29:18 +0200

    update CalcUi js and css

--- src/components/CalcUi.js
+++ src/components/CalcUi.js
@@ -2,103 +2,138 @@ import { useState } from 'react'
 import "./CalcUi.css";
 
 function CalcUi() {
+    const [screenValue, setScreenValue] = useState('')
+    function updateValueScreen(e) {
+        setScreenValue(screenValue + e.target.value)
+    }
+    function allClear() {
+        setScreenValue('')
+    }
+    function charDelete() {
+        setScreenValue(screenValue.slice(0, -1))
+    }
+    function getResult() {
+        setScreenValue(eval(screenValue))
+
+    }
     return (
-        <div>
+        <div className='calc-container'>
 
             <div>
-                <input className='screen' />
+                <input className='screen' value={screenValue} />
             </div>
+            <div className='btn-container'>
+                <div>
+                    <input
+                        type='button' value={"7"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-            <div>
-                <input
-                    type='button' value={"7"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"8"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"8"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"9"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"9"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"AC"} className='btn-calc'
+                        onClick={allClear}
+                    />
 
-                <input
-                    type='button' value={"AC"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"C"} className='btn-calc'
+                        onClick={charDelete}
+                    />
 
-                <input
-                    type='button' value={"C"} className='btn-calc'
-                />
+                </div>
 
-            </div>
+                <div>
+                    <input
+                        type='button' value={"4"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-            <div>
-                <input
-                    type='button' value={"4"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"5"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"5"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"6"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"6"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"+"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"+"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"-"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"-"} className='btn-calc'
-                />
+                </div>
 
-            </div>
+                <div>
+                    <input
+                        type='button' value={"1"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-            <div>
-                <input
-                    type='button' value={"1"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"2"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"2"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"3"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"3"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"*"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"*"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"/"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"/"} className='btn-calc'
-                />
+                </div>
 
-            </div>
+                <div>
+                    <input
+                        type='button' value={"00"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-            <div>
-                <input
-                    type='button' value={"00"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"0"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"0"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"."} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"."} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"000"} className='btn-calc'
+                        onClick={updateValueScreen}
+                    />
 
-                <input
-                    type='button' value={"()"} className='btn-calc'
-                />
+                    <input
+                        type='button' value={"="} className='btn-calc'
+                        onClick={getResult}
+                    />
 
-                <input
-                    type='button' value={"="} className='btn-calc'
-                />
-
+                </div>
             </div>
 
         </div>
